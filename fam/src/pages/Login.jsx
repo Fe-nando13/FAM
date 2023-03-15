a3ee8fb4b100736f33b08d0e156d53a01aac9c51
MariannaBR01 <124855269+MariannaBR01@users.noreply.github.com>
2023-03-15 20:00:40 +1100

    Update Login.jsx

--- fam/src/pages/Login.jsx
+++ fam/src/pages/Login.jsx
@@ -1,43 +1,68 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import loginList from "./loginList";
 
 export const Login = (props) => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
 
-const [email, setEmail] = useState("");
-const [pass, setPass] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, pass);
+    console.log(loginList);
 
-const handleSubmit = (e) => {
-e.preventDefault();
-console.log(email);
-};
-+9
-return (
-<>
-<form className="login-form">
-<label htmlFor="email">Email</label>
-<input
-value={email}
-type="email"
-placeholder="youremail@mail.com"
-id="email"
-name="email"
-/>
+    const emails = loginList.filter((item) => item.email === email);
+    console.log(emails);
+    if (emails.length === 0) {
+      //email is not in loginList
+      console.log("emailNaoencontrado");
+    } else {
+      const dataLogin = emails[0];
+      //checar se a senha esta certa
+      console.log(dataLogin);
 
-<label htmlFor="password">Password</label>
-<input
-value={pass}
-type="password"
-placeholder="**********"
-id="password"
-name="password"
-/>
-<button type="submit">Log In</button>
-</form>
-<button
-className="link-btn"
-onClick={() => props.onFormSwitch("register")}
->
-Don't have an account? Register here.
-</button>
-</>
-);
+      if (dataLogin.pass === pass) {
+        //senha correta
+        navigate("/Company");
+      } else {
+        //senha errada
+      }
+    }
+  };
+
+  return (
+    <>
+      <form className="login-form">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          placeholder="youremail@mail.com"
+          onChange={(e) => setEmail(e.target.value)}
+
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          placeholder="**********"
+          onChange={(e) => setPass(e.target.value)}
+          /*value={pass}*/
+          /*id="password"
+          name="password"*/
+        />
+        <button type="submit" onClick={handleSubmit}>
+          Log In
+        </button>
+      </form>
+      <h4>Do not have an account?</h4>
+      <button
+        className="link-btn"
+        onClick={() => props.onFormSwitch("register")}
+      >
+        Register here
+      </button>
+    </>
+  );
 };
